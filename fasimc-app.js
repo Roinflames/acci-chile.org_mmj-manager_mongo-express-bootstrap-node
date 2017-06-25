37e9764ecba673514b7d192407139b98c97e150c
roinflames <rod.reyes.s@gmail.com>
2017-06-25 16:04:50 -0400

    back to jade

--- fasimc-app.js
+++ fasimc-app.js
@@ -14,7 +14,7 @@ var port = 3000;
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
