107a64dea954968999b7edbab2c0b0608bd2c8eb
roinflames <rod.reyes.s@gmail.com>
2017-07-31 22:35:27 -0400

    fix index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'DM-ACCI' });
+  res.render('index', { title: 'ACCI' });
 });
 
 module.exports = router;
